8359f990c04c1834ead50792832904de4a41e648
arnedeboudt <arne.deboudt@student.ucll.be>
2025-12-15 14:54:10 +0100

    app.py

---
+++ app.java
@@ -0,0 +1,7 @@
+class App {
+    // Method named main denotes entry point
+    public static void main(String[] args) {
+        // Writes "Hello world!" to the screen
+        System.out.println("Hello world!");
+    }
+}
