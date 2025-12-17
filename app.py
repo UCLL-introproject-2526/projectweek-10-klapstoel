0a2b275e8d983fa4da800c9efa986e3ec7ac589b
EldarBv <eldar.eldarbaev@gmail.com>
2025-12-17 10:17:03 +0100

    Solutions

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+print('Hello world!')
+
+import pygame 
+pygame.init()
+
 import pygame
 
 
@@ -6,43 +11,22 @@ pygame.init()
 
 # Tuple representing width and height in pixels
 screen_size = (1024, 768)
-def create_main_surface():
-    # 1. INITIALISATIE EN VENSTER CREATIE (Dit moet EENMALIG gebeuren!)
-    import pygame
-    pygame.init() # Pygame moet eerst opgestart worden
-    
-    # DEFINIEER DE GROOTTE
-    screen_size = (800, 600) 
-    
-    # MAAK HET SCHERM AAN (UIT de loop!)
-    screen = pygame.display.set_mode(screen_size) 
-    
-    # Optioneel: Stel FPS in
-    clock = pygame.time.Clock()
-    
-    running = True
-    while running:
-        # Limit de snelheid (bijv. tot 60 frames per seconde)
-        clock.tick(60) 
 
-        # 1. EVENT HANDLING: Luister naar het besturingssysteem
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False  # De loop stopt
-        
-        # 2. GAME LOGIC: Beweeg objecten, bereken nieuwe posities
-        # ...
+# Create window with given size
+pygame.display.set_mode(screen_size)
 
-        # 3. RENDERING: Teken alles opnieuw op het scherm
-        screen.fill((255, 255, 255)) # Maak het scherm wit
-        # ... Teken hier je objecten ...
-        
-        # ESSENTIEEL: Update het scherm om veranderingen zichtbaar te maken
-        pygame.display.flip() 
-        
-    pygame.quit() # Sluit Pygame netjes af
+x = int(input("Please enter an integer: "))
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
 
-# Roep de functie aan om het spel te starten
-if __name__ == '__main__':
-    create_main_surface()
-pygame.quit() # Sluit Pygame netjes af
+    from pygame.display import flip
+
+# Copying back buffer to front buffer
+flip()  # Instead of pygame.display.flip()
